037f339220cd41ca21e70172a200d3f6e3fa02a9
Steve <steve21124@gmail.com>
2014-05-20 17:30:04 +0700

    update

--- Classes/ShareKit/Sharers/Services/Dropbox/SHKDropbox.h
+++ Classes/ShareKit/Sharers/Services/Dropbox/SHKDropbox.h
@@ -21,7 +21,7 @@
 static NSString *const kSHKDropboxDestinationDir __attribute__((deprecated ("use dropboxDestinationDirectory property of SHKItem instead. The value is available through [notification.object progress]"))) = @"SHKDropboxDestinationDir";
 //the key uses to send notifications with NSNotificationCenter
 static NSString *const kSHKDropboxUploadProgress __attribute__((deprecated ("use SHKSendProgressNotification instead."))) =@"SHKDropboxUploadProgress";
-static NSString *const kSHKDropboxSharableLink __attribute__((deprecated ("use userInfo payload of SHKSendDidFinishNotification instead"))) = @"SHKDropboxSharableLink"; 
+static NSString *const kSHKDropboxSharableLink __attribute__((deprecated ("use userInfo payload of SHKSendDidFinishNotification instead"))) = @"SHKDropboxSharableLink";
 
 @interface SHKDropbox : SHKSharer <DBNetworkRequestDelegate, DBRestClientDelegate, UIAlertViewDelegate, SHKFormOptionControllerOptionProvider>
 
